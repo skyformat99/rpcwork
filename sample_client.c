384c961d81cfb65eee24da75b043d7b49d96ebed
leonwu327 <wuxy327@126.com>
2017-10-15 03:08:57 -0400

    modify path

--- sample_client.c
+++ sample_client.c
@@ -1,11 +1,12 @@
 #include "rpc_private.h"
 
-int main () {
+
+static int sample_request(uint8_t opcode) {
     struct sd_req hdr = {0};
     struct sd_rsp rsp;
     int ret;
   
-	hdr.opcode = RPC_OP_TYPE_FIX;
+	hdr.opcode = opcode;
 	hdr.proto_ver = SD_PROTO_VER;
 
     ret = rpc_send_request(&hdr, &rsp, NULL);
@@ -13,5 +14,32 @@ int main () {
         return ret;
     }
     return rsp.result;
+}
+
+static void *test_loop(void *arg) {
+	(void)arg;
+    struct sd_req hdr;
+    int n = 10;
+	while (n --) {
+        sample_request(RPC_OP_TYPE_FIX);
+		sample_request(RPC_OP_TYPE_DYNAMIC);
+	}
+	return NULL;
+}
+
+int main () {
+    pthread_t thread1;
+	pthread_t thread2;
+	pthread_t thread3;
+	pthread_t thread4;
+    pthread_create(&thread1, NULL, test_loop, NULL);
+	pthread_create(&thread2, NULL, test_loop, NULL);
+	pthread_create(&thread3, NULL, test_loop, NULL);
+	pthread_create(&thread4, NULL, test_loop, NULL);
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+	pthread_join(thread3, NULL);
+	pthread_join(thread4, NULL);
+	return 0;
 }
 
